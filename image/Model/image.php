8744e75f4ccdae9f27a1dcc92e4dcba8a52ccb21
Humbert <moreaux.humbert@gmail.com>
2016-10-17 09:42:07 +0200

    db sqlite3 fonctionnelle

--- image/Model/image.php
+++ image/Model/image.php
@@ -2,12 +2,13 @@
   
     # Notion d'image
     class Image {
-        private $url = ""; 
         private $id  = 0;
+        private $url = "";
 
-        function __construct($u, $id) {
-            $this->url = $u;
+        function __construct($id, $url) {
             $this->id  = $id;
+            $this->url = $url;
+
         }
 
         # Retourne l'URL de cette image
