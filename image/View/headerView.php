b012557d95e53841cdf5dc8e3b76c23242e6145e
Humbert <moreaux.humbert@gmail.com>
2016-11-07 11:37:21 +0100

    New header site

---
+++ image/View/headerView.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand">Site SIL3</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> S'inscrire</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
